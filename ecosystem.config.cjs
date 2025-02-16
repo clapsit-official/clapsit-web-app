205e1b6b765249fb426f2d3ec051ef6f6125e974
Vugar Safarzada <vuqarsfrzad@gmail.com>
2025-02-17 02:32:54 +0400

    Changes

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -1,19 +1,15 @@
 module.exports = {
-    apps: [
-      {
-        name: "clapsit-web-app",
-        script: "pnpm",
-        args: "run deploy",
-        autorestart: true,
-        watch: false,
-        exec_mode: "fork",
-        env: {
-          NODE_ENV: "production",
-        },
-        post_update: [
-          "pnpm run deploy",
-        ],
+  apps: [
+    {
+      name: "clapsit-web-app",
+      script: "npm",
+      args: "run deploy",
+      autorestart: true,
+      watch: false,
+      exec_mode: "fork",
+      env: {
+        NODE_ENV: "production",
       },
-    ],
-  };
-  
+    },
+  ],
+};
